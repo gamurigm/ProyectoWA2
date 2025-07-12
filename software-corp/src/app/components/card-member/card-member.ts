ee92a53f84935f4b06e7c5ccd7959b72db935ae2
Pablo Zurita <pablozurita.jr@outlook.com>
2025-07-12 16:27:25 -0500

    Creación de Componentes

---
+++ software-corp/src/app/components/card-member/card-member.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-member',
+  imports: [],
+  templateUrl: './card-member.html',
+  styleUrl: './card-member.css'
+})
+export class CardMember {
+
+}
